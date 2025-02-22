387f7f2781e1a855489309b24394e9d6394ca1ee
CheleaMihail <mikeskin10@gmail.com>
2025-02-22 16:08:02 +0200

    add chessboard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ChessBoard from "../components/Chessboard";
 
 const Home = () => {
-  return <main className="container-fluid h-100 bg-transparent">Home</main>;
+  return (
+    <main className="container-fluid h-100 bg-transparent">
+      <h1 className="text-center my-4">Chess Game</h1>
+      <ChessBoard />
+    </main>
+  );
 };
 
 export default Home;
